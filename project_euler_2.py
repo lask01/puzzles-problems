49002ed6646b4cfe39c140e3c4d511ae77677a96
lask01 <38842343+lask01@users.noreply.github.com>
2019-10-22 22:55:34 -0400

    Add files via upload

---
+++ project_euler_2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+@author: lask01
+"""
+
+def dynamic_programming_solution(max_value):
+    """
+    Space efficient dynamic programming solution
+    """
+    
+    # For simplification
+    assert(max_value > 2)
+
+    sum_fib = 0    
+    n_minus_two = 0
+    n_minus_one = 1
+    n = n_minus_two + n_minus_one
+    
+    while n < max_value:
+         n = n_minus_two + n_minus_one
+         n_minus_two = n_minus_one
+         n_minus_one = n
+         if n%2 == 0:
+             sum_fib += n
+    return sum_fib
+
+if __name__=="__main__":
+    print(dynamic_programming_solution(4*(10**6)))
+    
